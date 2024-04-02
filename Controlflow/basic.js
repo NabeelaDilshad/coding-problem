64adf343c48615471510fbddfd5525ec7ef724a6
faizuddin53 <faizuddinansari53@gmail.com>
2024-04-02 23:43:18 +0530

    if else conditon

--- Controlflow/basic.js
+++ Controlflow/basic.js
@@ -1,1 +1,24 @@
 // learn basic control flow
+
+// if , else , else if , 
+
+
+function getLocation(location){
+    if(location === 'aligarh'){
+        //  console.log(`welcome you are in ${location}`)
+    }
+    else{
+        // console.log(`you are not in aligarh `)
+    }
+};
+
+getLocation('aligarh');
+getLocation('delhi');
+
+
+/* Quaetion -  age below 18 - your are miner
+  if age 18 se 25 - your are young focus your goals;
+  if age 25 to 40 - you have focus more time with family;
+  else  "do something"
+
+*/
