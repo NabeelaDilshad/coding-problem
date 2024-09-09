55f595f3e8a0f17aa2aea249c2db4223318236ca
nabeela <dilshadnabeela174@gmail.com>
2024-09-09 23:29:16 +0530

    change

--- BaseOfJs/basic.js
+++ BaseOfJs/basic.js
@@ -1,2 +1,12 @@
+// function add(num1, num2, num3) {
+//     return num1 + num2 + num3;
+// }
+// const r = add(3, 4, 7)
+// console.log(r);
+
+const addition = (num1, num2, num3) => num1 + num2 + num3
+
+const res = addition(3, 4, 7)
+console.log(res);
 
 
