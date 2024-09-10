f0eedac0e341b77c7d950e1808b8b3f18e266385
nabeela <dilshadnabeela174@gmail.com>
2024-09-10 15:35:59 +0530

    changes

--- Callback-Promise/promise.js
+++ Callback-Promise/promise.js
@@ -1,1 +1,41 @@
+const cart = ["pyjama", "shoes"];
+
+const promise = createORDER(cart);
+
+promise.then(function (orderId) {
+    console.log(orderId);
+    // proceedtoPayment(orderId)
+})
+
+    .catch(function (orderId) {
+        console.log("order id is not valid")
+    })
+function createORDER(cart) {
+    const pr = new Promise(function (resolve, reject) {
+
+        if (!validatecart(cart)) {
+            const err = new Error("cart is not valid")
+            reject(err);
+        }
+
+        //logic for create order
+        const orderId = 1234556;
+        if (orderId) {
+            setTimeout(function () {
+                resolve(orderId);
+            }, 5000)
+        }
+
+        return;
+
+    })
+    return pr;
+}
+function validatecart(cart) {
+    return true;
+}
+
+
+
+
 
