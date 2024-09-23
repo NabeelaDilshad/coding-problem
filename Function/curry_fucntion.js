abb6203dd906416186deb6ffc9068480556c1078
faizuddin53 <faizuddinansari53@gmail.com>
2024-09-23 22:59:58 +0530

    fun

---
+++ Function/curry_fucntion.js
@@ -0,0 +1,20 @@
+
+
+// function a(){
+//      return function(){
+//           return function(){
+//               return function(){
+//                   console.log("hey")
+//               }
+//           }
+//      }
+// }
+
+//  a()()()();
+
+
+function sum(){
+
+}
+
+sum(10)(20)(30)(40)(50) // 150
