8d4ce190bc67d7238e4442d11aabb0d8922e305d
faizuddin53 <faizuddinansari53@gmail.com>
2024-12-29 22:57:32 +0530

    till hoisting

--- Set_Map/Map.js
+++ Set_Map/Map.js
@@ -29,3 +29,11 @@ map1.set(1, 'bar');
 for( let [ phla , dushra ] of map1.entries()){
       console.log(phla , dushra)
 }
+
+const map2 = new Map();
+map2.set('0', 'foo');
+map2.set(1, 'bar');
+
+for( let item of map2.values()) {
+//     console.log(item) // foo , bar
+}
