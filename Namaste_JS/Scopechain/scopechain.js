5e9eb7f2a414379a674d6272b4230d080039c06c
faizuddin53 <faizuddinansari53@gmail.com>
2024-12-30 22:14:07 +0530

    TDZ

---
+++ Namaste_JS/Scopechain/scopechain.js
@@ -0,0 +1,38 @@
+
+
+console.log(a) 
+var aa = 10;
+
+function getuser(){
+     let name = "faizuddin"
+     console.log("hey user")
+     function otheruser(){
+            // local memmory and parent of his laxial parent  
+            let username = "otheruser"
+            let price = 40;
+            console.log(username)
+            console.log(name)
+            function thirdfunction(){
+                let address = 'aligarh'
+                console.log("i am from " + address, a, username)
+            }
+            thirdfunction();
+
+     }
+     otheruser()
+}
+
+getuser();
+
+
+
+function ab(){
+     var b =10;
+     c();
+     function c(){
+          console.log(b);
+     }
+}
+
+ab()
+
